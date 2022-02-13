bc9149a07858ae1fecd2bb2045cd2321ac876e0a
xxxRichiexxx <xxxRichiexxx@yandex.ru>
2022-02-13 21:45:10 +0300

    Спрятал ключ.

--- APPLICATION/Helpdesk/settings.py
+++ APPLICATION/Helpdesk/settings.py
@@ -10,8 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 from datetime import timedelta
-
 from pathlib import Path
+import os
+
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a%5sd5f!f=aw)9a@tbz1-f3nb*($a(#6+&^9plzp3p_tzw%$nx'
+load_dotenv()
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
